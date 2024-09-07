bb894a31f01025d3affd08f32d8f011e90c937d6
allanwang <allan357161@gmail.com>
2024-09-07 13:33:34 -0700

    Initial Commit

---
+++ AlgorithmVisualizer/AlgorithmVisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlgorithmVisualizerApp.swift
+//  AlgorithmVisualizer
+//
+//  Created by Allan Wang on 2024-09-07.
+//
+
+import SwiftUI
+
+@main
+struct AlgorithmVisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
