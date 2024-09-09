ca0fefd3987890ac19a8777100c56e020653e680
allanwang <allan357161@gmail.com>
2024-09-09 11:29:43 -0700

    Created a grid represented by buttons, each has label of a rectangle. Will alter the colour of the rectangle once it is clicked.

--- AlgorithmVisualizer/ContentView.swift
+++ AlgorithmVisualizer/ContentView.swift
@@ -1,23 +1,53 @@
-//
-//  ContentView.swift
-//  AlgorithmVisualizer
-//
-//  Created by Allan Wang on 2024-09-07.
-//
 
 import SwiftUI
 
 struct ContentView: View {
+    let square = Image(systemName:"square")
+    let size: CGFloat = 30
+    var COLS = 10
+    var ROWS = 5
+    @State private var colours: [[Color]]
+    
+    init() {
+        _colours = State(initialValue: Array(repeating: (Array(repeating: Color.white, count: COLS)), count: ROWS))
+    }
+    
     var body: some View {
+        
+        let MAPWIDTH: CGFloat = CGFloat(CGFloat(COLS) * size)
+        let MAPHEIGHT: CGFloat = CGFloat(CGFloat(ROWS) * size)
+                   
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack(spacing: 0) {
+                ForEach(0..<ROWS, id:\.self) { col in
+                    HStack(spacing:0) {
+                        ForEach(0..<COLS, id:\.self) { row in
+                            
+                            // each square in the grid is an individual button
+                            Button {
+                                changeWall(col, row, &colours)
+                            } label: {
+                                Rectangle()
+                                    .fill(colours[col][row])
+                                    .frame(width: size, height: size)
+                                    .border(Color.black, width:1)
+                            }
+                        }
+                    }
+                }
+            }
         }
-        .padding()
+        .frame(width: MAPWIDTH, height: MAPHEIGHT)
+        .border(Color.black, width: 2)
+        
+    }
+    func changeWall(_ x: Int, _ y: Int, _ colours: inout [[Color]]) {
+        colours[x][y] = Color.black
     }
 }
+
+
 
 #Preview {
     ContentView()
