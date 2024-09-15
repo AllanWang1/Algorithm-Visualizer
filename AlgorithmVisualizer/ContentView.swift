422f977be92d789c419c61e1335999c55bf823e6
allanwang <allan357161@gmail.com>
2024-09-14 23:53:50 -0700

    Implemented random maze generating algorithm. Refactored start and target into tuples instead of 2D array to prevent unpredicted behaviour by fixing the size. Added clearAll() feature.

--- AlgorithmVisualizer/ContentView.swift
+++ AlgorithmVisualizer/ContentView.swift
@@ -3,15 +3,15 @@ import SwiftUI
 
 struct ContentView: View {
     let square = Image(systemName:"square")
-    let size: CGFloat = 30
-    var COLS = 30
-    var ROWS = 20
+    let size: CGFloat = 20
+    var COLS = 40
+    var ROWS = 28
     @State private var colours: [[Color]]
     
     @State var settingStart:Bool = false
-    @State var start: [Int] = []
+    @State var start: (Int, Int) = (-1, -1)
     @State var settingTarget:Bool = false
-    @State var target: [Int] = []
+    @State var target: (Int, Int) = (-1, -1)
     
     init() {
         _colours = State(initialValue: Array(repeating: (Array(repeating: Color.white, count: ROWS)), count: COLS))
@@ -33,7 +33,7 @@ struct ContentView: View {
                             
                             // each square in the grid is an individual button
                             Button {
-                                changeWall(col, row, &colours)
+                                changeWall(col, row)
                                 clearSolution()
                             } label: {
                                 Rectangle()
@@ -47,45 +47,49 @@ struct ContentView: View {
             }
             .frame(width: MAPWIDTH, height: MAPHEIGHT)
             .border(Color.black, width: 2)
-            // set start button
-            Button {
-                settingStart = !settingStart
-                if (settingTarget) {
-                    settingTarget = false
+            HStack {
+                
+                // set start button
+                Button {
+                    settingStart = !settingStart
+                    if (settingTarget) {
+                        settingTarget = false
+                    }
+                } label: {
+                    if (settingStart) {
+                        Text("Setting Start Position...")
+                            .font(.system(size: 24))
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.blue)
+                    } else {
+                        Text("Set Start Position")
+                            .font(.system(size: 24))
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.blue)
+                    }
                 }
-            } label: {
-                if (settingStart) {
-                    Text("Setting Start Position...")
-                        .font(.system(size: 24))
-                        .fontWeight(.bold)
-                        .foregroundColor(Color.blue)
-                } else {
-                    Text("Set Start Position")
-                        .font(.system(size: 24))
-                        .fontWeight(.bold)
-                        .foregroundColor(Color.blue)
+                
+                // set target button
+                Button {
+                    settingTarget = !settingTarget
+                    if (settingStart) {
+                        settingStart = false
+                    }
+                } label: {
+                    if (settingTarget) {
+                        Text("Setting Target Position...")
+                            .font(.system(size: 24))
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.blue)
+                    } else {
+                        Text("Set Target Position")
+                            .font(.system(size: 24))
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.blue)
+                    }
                 }
+                
             }
-            // set target button
-            Button {
-                settingTarget = !settingTarget
-                if (settingStart) {
-                    settingStart = false
-                }
-            } label: {
-                if (settingTarget) {
-                    Text("Setting Target Position...")
-                        .font(.system(size: 24))
-                        .fontWeight(.bold)
-                        .foregroundColor(Color.blue)
-                } else {
-                    Text("Set Target Position")
-                        .font(.system(size: 24))
-                        .fontWeight(.bold)
-                        .foregroundColor(Color.blue)
-                }
-            }
-            
             
             HStack {
                 Spacer()
@@ -120,104 +124,143 @@ struct ContentView: View {
             }
             .foregroundColor(Color(red: 152/255, green: 200/255, blue: 151/255))
             
-            Button {
-                clearSolution()
-            } label: {
-                ZStack {
-                    Rectangle()
-                        .frame(width: 180, height: 70)
-                        .cornerRadius(15)
-                    Text("Clear Solution")
-                        .font(.title)
-                        .foregroundColor(Color.black)
+            HStack {
+                Button {
+                    var newMaze: [[Color]] = Array(repeating: Array(repeating: Color.black, count: ROWS), count: COLS)
+                    generateUniqueMaze(&newMaze, (1, 1))
+                    colours = newMaze
+                } label: {
+                    ZStack {
+                        Rectangle()
+                            .frame(width: 180, height: 70)
+                            .cornerRadius(15)
+                        Text("Generate \nUnique Maze")
+                            .font(.title)
+                            .foregroundColor(Color.black)
+                    }
+                    .foregroundColor(Color(red: 255/255, green: 200/255, blue: 150/255))
                 }
-                .foregroundColor(Color(red: 255/255, green: 200/255, blue: 150/255))
+                
+                Button {
+                    clearSolution()
+                } label: {
+                    ZStack {
+                        Rectangle()
+                            .frame(width: 180, height: 70)
+                            .cornerRadius(15)
+                        Text("Clear Solution")
+                            .font(.title)
+                            .foregroundColor(Color.black)
+                    }
+                    .foregroundColor(Color(red: 255/255, green: 200/255, blue: 150/255))
+                }
             }
         }
         
     }
-    func changeWall(_ x: Int, _ y: Int, _ colours: inout [[Color]]) {
-        print("solution cleared")
+    func changeWall(_ x: Int, _ y: Int) {
+        
         if (settingStart) {
-            if (target != [x, y]) {
-                if (start.count != 0) {
-                    colours[start[0]][start[1]] = Color.white
+            if (target != (x, y)) {
+                if (start != (-1, -1)) {
+                    colours[start.0][start.1] = Color.white
                 }
                 colours[x][y] = Color.cyan
-                start = [x, y]
+                start = (x, y)
             }
         }
         else if (settingTarget) {
-            if (start != [x, y]) {
-                if (target.count != 0) {
-                    colours[target[0]][target[1]] = Color.white
+            if (start != (x, y)) {
+                if (target != (-1, -1)) {
+                    colours[target.0][target.1] = Color.white
                 }
                 colours[x][y] = Color.red
-                target = [x, y]
+                target = (x, y)
             }
         }
         
-        else if (colours[x][y] == Color.white) {
+        else if (colours[x][y] == Color.black) {
             // deal with start and target blocks
-            if (start == [x, y]) {start = []}
-            if (target == [x, y]) {target = []}
-            colours[x][y] = Color.black
+            if (start == (x, y)) {start = (-1, -1)}
+            if (target == (x, y)) {target = (-1, -1)}
+            colours[x][y] = Color.white
         } else {
             // deal with start and target blocks
-            if (start == [x, y]) {start = []}
-            if (target == [x, y]) {target = []}
-            colours[x][y] = Color.white
+            if (start == (x, y)) {start = (-1, -1)}
+            if (target == (x, y)) {target = (-1, -1)}
+            colours[x][y] = Color.black
+        }
+    }
+    
+    //var newMaze: [[Color]] = Array(repeating: Array(repeating: Color.black, count: ROWS), count: COLS)
+    func generateUniqueMaze(_ maze: inout [[Color]], _ curr: (Int, Int)) {
+        clearAll()
+        let x: Int = curr.0
+        let y: Int = curr.1
+        maze[x][y] = Color.white
+        
+        let directions = [(0, 2), (2, 0), (0, -2), (-2, 0)]
+        for direction in directions.shuffled() {
+            let newx = x + direction.0
+            let newy = y + direction.1
+            
+            if (newx > 0 && newx < COLS && newy > 0 && newy < ROWS) {
+                if (maze[newx][newy] == Color.black) {
+                    maze[(x + newx)/2][(y + newy)/2] = Color.white
+                    generateUniqueMaze(&maze, (newx, newy))
+                }
+            }
         }
     }
     
     func BFS() {
         // Guard in case the start or the target has not been set yet
-        if (target.count != 2 || start.count != 2) {
+        if (target == (-1, -1) || start == (-1, -1)) {
             return
         }
         clearSolution()
         var visited: [[Bool]] = Array(repeating: Array(repeating: false, count: ROWS), count: COLS)
         
-        var toVisit = Queue<[Int]>()
+        let toVisit = Queue<(Int, Int)>()
         
         var parents = Array(repeating: Array(repeating: (-1, -1), count: ROWS), count: COLS)
         
         toVisit.enqueue(start)
-        visited[start[0]][start[1]] = true
+        visited[start.0][start.1] = true
         
         while (!toVisit.isEmpty) {
-            let curr: [Int] = toVisit.front!
-            let x = curr[0]
-            let y = curr[1]
-            if x == target[0] && y == target[1] {
+            let curr: (Int, Int) = toVisit.front!
+            let x = curr.0
+            let y = curr.1
+            if x == target.0 && y == target.1 {
                 print("found target")
                 backtrack(parents)
-                //print(String(x) + ", " + String(y))
                 break
             }
             toVisit.dequeue()
             // produce neighbours
-            let neighbours: [[Int]] = [[x + 1, y],
-                                       [x - 1, y],
-                                       [x, y + 1],
-                                       [x, y - 1]]
+            let neighbours: [(Int, Int)] = [(x, y + 1),
+                                            (x + 1, y),
+                                            (x, y - 1),
+                                            (x - 1, y)]
             // if not out of bounds, wall, or visited, add to toVisit
             for point in neighbours {
-                let i = point[0]
-                let j = point[1]
+                let i = point.0
+                let j = point.1
                 if (good(visited, point)) {
                     visited[i][j] = true
-                    if (i != target[0] || j != target[1]) {colours[i][j] = Color.purple}
+                    if (i != target.0 || j != target.1) {colours[i][j] = Color.purple}
                     parents[i][j] = (x, y)
                     toVisit.enqueue(point)
                 }
             }
         }
+        noSolution()
     }
     
-    func good(_ visited: [[Bool]], _ point: [Int]) -> Bool {
-        let x = point[0]
-        let y = point[1]
+    func good(_ visited: [[Bool]], _ point: (Int, Int)) -> Bool {
+        let x = point.0
+        let y = point.1
         // Check out of bounds before checking array to avoid out of bounds error.
         if 0 <= x && x < COLS && 0 <= y && y < ROWS {
             if !visited[x][y] && colours[x][y] != Color.black{
@@ -237,8 +280,8 @@ struct ContentView: View {
             }
         }
         var path: [(Int, Int)] = []
-        var curr: (Int, Int) = parents[target[0]][target[1]]
-        while (curr != (start[0], start[1])) {
+        var curr: (Int, Int) = parents[target.0][target.1]
+        while (curr != (start.0, start.1)) {
             path.insert(curr, at: 0)
             print(String(curr.0) + ", " + String(curr.1))
             curr = parents[curr.0][curr.1]
@@ -248,14 +291,26 @@ struct ContentView: View {
         }
     }
     
+    func noSolution() {
+        
+    }
+    
     func clearSolution() {
         for x in 0..<COLS {
             for y in 0..<ROWS {
-                if (colours[x][y] != Color.black && start != [x,y] && target != [x,y]) {
+                if (colours[x][y] != Color.black && start != (x, y) && target != (x, y)) {
                     colours[x][y] = Color.white
                 }
             }
         }
+    }
+    
+    func clearAll() {
+        settingStart = false
+        start = (-1, -1)
+        settingTarget = false
+        target = (-1, -1)
+        colours = Array(repeating: Array(repeating: Color.white, count: ROWS), count: COLS)
     }
 }
 
