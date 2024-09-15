107870ca33cec230ca1752d5eab7a879dfec6f97
allanwang <allan357161@gmail.com>
2024-09-14 18:58:36 -0700

    Refactored arrays and array lookups, such that point[0] can refer to x, and point[1] can refer to y, and colours[x][y] can now correctly refer to the points as shown directly on the maze.

--- AlgorithmVisualizer/ContentView.swift
+++ AlgorithmVisualizer/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
     @State var target: [Int] = []
     
     init() {
-        _colours = State(initialValue: Array(repeating: (Array(repeating: Color.white, count: COLS)), count: ROWS))
+        _colours = State(initialValue: Array(repeating: (Array(repeating: Color.white, count: ROWS)), count: COLS))
     }
     
     var body: some View {
@@ -27,9 +27,9 @@ struct ContentView: View {
             Spacer()
             
             VStack(spacing: 0) {
-                ForEach(0..<ROWS, id:\.self) { col in
+                ForEach(0..<ROWS, id:\.self) { row in
                     HStack(spacing:0) {
-                        ForEach(0..<COLS, id:\.self) { row in
+                        ForEach(0..<COLS, id:\.self) { col in
                             
                             // each square in the grid is an individual button
                             Button {
@@ -119,12 +119,12 @@ struct ContentView: View {
             }
             .foregroundColor(Color(red: 152/255, green: 200/255, blue: 151/255))
             
-            
             Spacer()
         }
         
     }
     func changeWall(_ x: Int, _ y: Int, _ colours: inout [[Color]]) {
+        
         if (settingStart) {
             if (target != [x, y]) {
                 if (start.count != 0) {
@@ -162,21 +162,20 @@ struct ContentView: View {
         if (target.count != 2 || start.count != 2) {
             return
         }
-        var delayInSeconds = 0.05
-        var visited: [[Bool]] = Array(repeating: Array(repeating: false, count: COLS), count: ROWS)
+        var visited: [[Bool]] = Array(repeating: Array(repeating: false, count: ROWS), count: COLS)
         
         var toVisit = Queue<[Int]>()
         
-        var parents = Array(repeating: Array(repeating: (-1, -1), count: COLS), count: ROWS)
+        var parents = Array(repeating: Array(repeating: (-1, -1), count: ROWS), count: COLS)
         
         toVisit.enqueue(start)
         visited[start[0]][start[1]] = true
         
         while (!toVisit.isEmpty) {
             let curr: [Int] = toVisit.front!
-            let x = curr[1]
-            let y = curr[0]
-            if x == target[1] && y == target[0] {
+            let x = curr[0]
+            let y = curr[1]
+            if x == target[0] && y == target[1] {
                 print("found target")
                 backtrack(parents)
                 //print(String(x) + ", " + String(y))
@@ -184,23 +183,18 @@ struct ContentView: View {
             }
             toVisit.dequeue()
             // produce neighbours
-            let neighbours: [[Int]] = [[y + 1, x],
-                                       [y - 1, x],
-                                       [y, x + 1],
-                                       [y, x - 1]]
+            let neighbours: [[Int]] = [[x + 1, y],
+                                       [x - 1, y],
+                                       [x, y + 1],
+                                       [x, y - 1]]
             // if not out of bounds, wall, or visited, add to toVisit
             for point in neighbours {
                 let i = point[0]
                 let j = point[1]
                 if (good(visited, point)) {
                     visited[i][j] = true
-//                    DispatchQueue.main.asyncAfter(deadline: .now() + delayInSeconds) {
-//                        if (i != target[0] || j != target[1]) {colours[i][j] = Color.purple}
-//                        
-//                    }
                     if (i != target[0] || j != target[1]) {colours[i][j] = Color.purple}
                     parents[i][j] = (x, y)
-                    delayInSeconds += 0.01
                     toVisit.enqueue(point)
                 }
             }
@@ -208,11 +202,11 @@ struct ContentView: View {
     }
     
     func good(_ visited: [[Bool]], _ point: [Int]) -> Bool {
-        let x = point[1]
-        let y = point[0]
+        let x = point[0]
+        let y = point[1]
         // Check out of bounds before checking array to avoid out of bounds error.
         if 0 <= x && x < COLS && 0 <= y && y < ROWS {
-            if !visited[y][x] && colours[y][x] != Color.black{
+            if !visited[x][y] && colours[x][y] != Color.black{
                 return true
             }
         }
@@ -230,13 +224,13 @@ struct ContentView: View {
         }
         var path: [(Int, Int)] = []
         var curr: (Int, Int) = parents[target[0]][target[1]]
-        while (curr != (start[1], start[0])) {
+        while (curr != (start[0], start[1])) {
             path.insert(curr, at: 0)
-            print(String(curr.1) + ", " + String(curr.0))
-            curr = parents[curr.1][curr.0]
+            print(String(curr.0) + ", " + String(curr.1))
+            curr = parents[curr.0][curr.1]
         }
         for cord in path {
-            colours[cord.1][cord.0] = Color.orange
+            colours[cord.0][cord.1] = Color.orange
         }
     }
 }
